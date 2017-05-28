89f8bcfa51529a87c8f473bdbf9bbd7661c4cc91
Alex de Kruijff <alex.de.kruijff@MazarineBlue.org>
2017-05-28 21:55:54 +0200

    Adding DummyFeed and TestInvokerSpy

---
+++ MazarineBlue-Engine/src/test/java/org/mazarineblue/libraries/TestInvokerSpy.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017 Alex de Kruijff <alex.de.kruijff@MazarineBlue.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package org.mazarineblue.libraries;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.mazarineblue.eventbus.Event;
+import org.mazarineblue.eventdriven.Chain;
+import org.mazarineblue.eventdriven.Interpreter;
+import org.mazarineblue.eventdriven.Invoker;
+
+class TestInvokerSpy
+        implements Invoker {
+
+    private List<Event> events = new ArrayList<>();
+
+    @Override
+    public Interpreter interpreter() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Chain chain() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void publish(Event event) {
+        events.add(event);
+    }
+
+    public List<Event> getEvents() {
+        return events;
+    }
+}
